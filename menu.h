07f3264c5a371304385ec917c6666e9d9ea87336
Salah1779 <147154837+Salah1779@users.noreply.github.com>
2024-03-17 14:32:32 +0000

    Update menu.h

--- menu.h
+++ menu.h
@@ -4,6 +4,17 @@
 
 // Structure pour les éléments d'un menu ou sous-menu
 
+// Définition des constantes pour les types d'éléments de menu
+#define DEFAULT_MENU 0
+#define CHECKBOX_MENU 1
+#define SEPARATOR_MENU 2
+#define IMAGE_MENU 3
+#define RADIO_MENU 4
+
+//Definir les constantes pour l'orientaton
+#define ORIENTATION_H 0
+#define ORIENTATION_V 1
+
 
 typedef struct Menu {
     GtkWidget *MENU;         // Contient le sous-menu associé à l'élément du menu
@@ -16,15 +27,6 @@ typedef struct Menu {
     struct Menu *liste;      // Liste des éléments de sous-menu s'il s'agit d'un menu
 } menu;
 
-menu* RadioGroup(menu* M)
-{
-  menu *courant;
-  for (courant = M; courant; courant = courant->suivant)
-     if(courant->IsradioGroup)
-        return courant;
-
- return NULL;
-}
 // Structure pour la barre de menus
 typedef struct {
     GtkWidget *menubar;// Contient la barre de menus GTK
@@ -121,6 +123,15 @@ void ajouter_menu(menubar *M, menu *menu_child)
     gtk_menu_shell_append(GTK_MENU_SHELL(M->menubar), menu_child->menu_item);
 }
 
+menu* RadioGroup(menu* M)
+{
+  menu *courant;
+  for (courant = M; courant; courant = courant->suivant)
+     if(courant->IsradioGroup)
+        return courant;
+
+ return NULL;
+}
 // Fonction pour ajouter un élément de menu à un menu parent
 void ajouter_menu_item(menu *parent_menu, menu *menu_elem, const gchar *icon)
 {
@@ -150,17 +161,7 @@ void ajouter_menu_item(menu *parent_menu, menu *menu_elem, const gchar *icon)
                 break;
             case 3:
                   if (icon) {
-                    // Chargement de l'icône prédéfinie à partir du thème d'icônes par défaut
-                    GtkIconTheme *icon_theme = gtk_icon_theme_get_default();
-                    GdkPixbuf *icon_pixbuf = gtk_icon_theme_load_icon(icon_theme, icon, GTK_ICON_SIZE_MENU, GTK_ICON_LOOKUP_USE_BUILTIN, NULL);
-
-                    // Créer le widget de l'élément de menu avec l'icône chargée
-                    menu_elem->menu_item = gtk_image_menu_item_new_with_mnemonic(menu_elem->nom);
-                    GtkWidget *icon_image = gtk_image_new_from_pixbuf(icon_pixbuf);
-                    gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(menu_elem->menu_item), icon_image);
-
-                    // Libérer la mémoire allouée pour le pixbuf de l'icône
-                    g_object_unref(icon_pixbuf);
+                    //image
                 } else {
                     // Créer un élément de menu sans icône
                     menu_elem->menu_item = gtk_menu_item_new_with_mnemonic(menu_elem->nom);
@@ -169,7 +170,7 @@ void ajouter_menu_item(menu *parent_menu, menu *menu_elem, const gchar *icon)
             case 4:
                 // Créer un bouton radio
                 menu* Rgroup = RadioGroup(parent_menu->liste);
-                if (!parent_menu->liste)
+                if ((!parent_menu->liste) ||(!Rgroup))
                     menu_elem->menu_item = gtk_radio_menu_item_new_with_mnemonic(NULL, menu_elem->nom);
                 else if(Rgroup)
                     menu_elem->menu_item = gtk_radio_menu_item_new_with_mnemonic_from_widget(GTK_RADIO_MENU_ITEM(Rgroup->menu_item), menu_elem->nom);
@@ -190,80 +191,36 @@ void ajouter_menu_item(menu *parent_menu, menu *menu_elem, const gchar *icon)
     gtk_menu_shell_append(GTK_MENU_SHELL(parent_menu->MENU), menu_elem->menu_item);
 }
 
-int main(int argc, char *argv[]) {
-    gtk_init(&argc, &argv);
 
-    // Création de la fenêtre principale
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Menu Example");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
-
-     GtkWidget *Fixed = gtk_fixed_new();
-    gtk_container_add(GTK_CONTAINER(window), Fixed);
-
+// Fonction pour créer et ajouter une barre de menus à un conteneur GtkFixed
+menubar *add_menu_bar(GtkWidget *parent_container, gint orientation) {
     // Création de la barre de menus
-    menubar *maMenuBar = creer_menubar(0, Fixed);
+    menubar *menu_bar = creer_menubar(orientation, parent_container);
 
-    // Ajout de la barre menu au conteneur GtkFixed
-   gtk_fixed_put(GTK_FIXED(Fixed), maMenuBar->menubar, 0, 0);
+    // Ajout de la barre de menus au conteneur GtkFixed
+    gtk_fixed_put(GTK_FIXED(parent_container), menu_bar->menubar, 0, 0);
 
-    // Exemple d'ajout d'un menu "File"
-    menu *menu_file = init_menu_item(0, "File", TRUE,FALSE);
-    ajouter_menu(maMenuBar, menu_file);
+    // Retour de la barre de menus nouvellement créée
+    return menu_bar;
+}
 
-    // Exemple d'ajout d'éléments de menu à "File"
-    menu *menu_open = init_menu_item(0, "_Open", TRUE,FALSE);
-    ajouter_menu_item(menu_file, menu_open,NULL);
+menu *add_menu(menubar *mbar, gchar *label)
+{
+    // Initialisation de l'élément de menu
+    menu *new_menu_item = init_menu_item(0, label,TRUE, FALSE);
+    // Ajout de l'élément de menu au menu parent
+    ajouter_menu(mbar, new_menu_item);
+    // Retour de le menu nouvellement créé
+    return new_menu_item;
+}
 
-        menu *menu_here = init_menu_item(1, "project", FALSE,FALSE);
-        ajouter_menu_item(menu_open, menu_here,NULL);
-          menu *menu_sep = init_menu_item(2,"sep" , FALSE,FALSE);
-          ajouter_menu_item(menu_open, menu_sep,NULL);
-           menu *menu_there = init_menu_item(0,"file" ,TRUE,FALSE);
-          ajouter_menu_item(menu_open, menu_there,NULL);
-
-    menu *menu_save = init_menu_item(1, "_Save", FALSE,FALSE);
-    ajouter_menu_item(menu_file, menu_save, NULL);
-
-    // Exemple d'ajout d'un menu "Edit"
-    menu *menu_edit = init_menu_item(0, "Edit", TRUE,FALSE);
-    ajouter_menu(maMenuBar, menu_edit);
-
-    // Exemple d'ajout d'éléments de menu à "Edit"
-    menu *menu_cut = init_menu_item(1, "C_ut",FALSE ,FALSE);
-    ajouter_menu_item(menu_edit, menu_cut, NULL);
-
-    menu *menu_copy = init_menu_item(3, "_Copy",FALSE ,FALSE);
-    ajouter_menu_item(menu_edit, menu_copy, "edit-copy");
-
-    menu *menu_paste = init_menu_item(3, "_Paste",FALSE,FALSE);
-    ajouter_menu_item(menu_edit, menu_paste, "edit-paste");
-
-     menu *menu_S = init_menu_item(0, "Language", TRUE,FALSE);
-    ajouter_menu(maMenuBar, menu_S);
-     menu *menu_B = init_menu_item(0, "Build", TRUE,FALSE);
-    ajouter_menu(maMenuBar, menu_B);
-    menu *menu_D = init_menu_item(0, "Debug", TRUE,FALSE);
-    ajouter_menu(maMenuBar, menu_D);
-     menu *menu_Set= init_menu_item(0, "Settings", TRUE,FALSE);
-    ajouter_menu(maMenuBar, menu_Set);
-    menu *menu_Help = init_menu_item(0, "Help", TRUE,FALSE);
-    ajouter_menu(maMenuBar, menu_Help);
-
-
-    gtk_container_add(GTK_CONTAINER(window), Fixed);
-
-
-
-
-    // Connexion du signal destroy pour quitter l'application
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-    // Affichage de la fenêtre et exécution de la boucle principale GTK
-    gtk_widget_show_all(window);
-    gtk_main();
-
-    return 0;
+menu *add_menu_item(menu *parent_menu, guint type, gchar *label, gboolean isSubmenu, gboolean isRadioGroup,  gchar *icon) {
+    // Initialisation de l'élément de menu
+    menu *new_menu_item = init_menu_item(type, label, isSubmenu, isRadioGroup);
+    // Ajout de l'élément de menu au menu parent
+    ajouter_menu_item(parent_menu, new_menu_item, icon);
+    // Retour de l'élément de menu nouvellement créé
+    return new_menu_item;
 }
 
 
