09d4ffbd34beb3687e1c802615fd37daf765bf6e
moujahid20 <147153819+moujahid20@users.noreply.github.com>
2024-03-24 21:56:54 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include "headers.h"
-
-
 int main(int argc, char *argv[]) {
     debut_programme(argc,argv);
      FILE *F = fopen("reader.txt", "r"); // Ouvre le fichier en mode lecture
     if (F == NULL) {
         g_print("Impossible d'ouvrir le fichier!!\n");
      }
+     printf("\n333");
     Fenetre *Win=(Fenetre*)malloc(sizeof(Fenetre));
     if(Win)
         {
@@ -137,3 +136,135 @@ int main(int argc, char *argv[]) {
 
 
 
+
+
+//
+//int main(int argc, char *argv[]) {
+//    debut_programme(argc,argv);
+//    Fenetre* ma_fenetre=add_window("ma fenetre","gtklogo.png","window","white",
+//                   3840,2160,GTK_WIN_POS_CENTER,
+//                   -1,-1,5,TRUE);
+//
+//    GtkWidget *notebook = exemple(ma_fenetre->window);
+//     // FILE* F=fopen("reader.txt","r");
+//     /// fixed* fixed0=init_gtk_fixed("fixed0");
+//     // GtkWidget* box=gtk_vbox_new(FALSE,10);
+//     // lire_fichier(F,fixed0,box,ma_fenetre);
+//     // gtk_box_pack_start(GTK_BOX(box), fixed0->fixed, FALSE, FALSE,10);
+////    GtkWidget* fixed1 = create_fixed();///fiha le menu bohdo db
+////    add_progress_bar(fixed1,2.3,1.0,1000,500,1,250,250);
+////    add_image(fixed1,1,"gtklogo.png",GTK_ICON_SIZE_BUTTON,50,50,250,250);
+////    add_onglet(notebook,fixed1,"onglet 2","#00FFFF");///pour ajouter le fixed dans l'onglet (tqdr dir bo wla tableau wla contenair, ay widget)
+////
+////    GtkWidget* fixed2 = create_fixed();///khawya
+////    texte* txt=initialiser_texte(20,30,"exemple frame",NULL,NULL,25,NULL,NULL,"red","#00FF00",NULL);
+////    add_label(fixed2,txt,TRUE,FALSE,350,350);
+////    add_onglet(notebook,fixed2,"onglet 3","#FFFF00");///pour ajouter le fixed dans l'onglet (tqdr dir bo wla tableau wla contenair, ay widget)
+/////*************************************frame example******************************************************************/
+////
+////
+////    GtkWidget* fixed3 = create_fixed();///khawya
+////
+////
+//////    add_cocher(fixed3,"testing cocher1",0,300,"#000000",TRUE);
+//////    add_cocher(fixed3,"testing cocher2",0,330,"#FFFFFF",FALSE);
+//////    add_cocher(fixed3,"testing cocher3",0,360,"#00FF00",TRUE);
+//////    add_cocher(fixed3,"testing cocher4",0,390,"#FF0000",FALSE);
+//////    add_cocher(fixed3,"testing cocher1",200,300,"#FFFFFF",TRUE);
+//////    add_cocher(fixed3,"testing cocher2",200,330,"#FFFFFF",TRUE);
+//////    add_cocher(fixed3,"testing cocher3",200,360,"#FFFFFF",TRUE);
+//////    add_cocher(fixed3,"testing cocher4",200,390,"#FFFFFF",TRUE);
+//////    add_cocher(fixed3,"testing cocher1",400,300,"#000000",FALSE);
+//////    add_cocher(fixed3,"testing cocher2",400,330,"#000000",FALSE);
+//////    add_cocher(fixed3,"testing cocher3",400,360,"#000000",FALSE);
+//////    add_cocher(fixed3,"testing cocher4",400,390,"#000000",FALSE);
+////    char *labels4[NB_RADIO] = {"Option 1", "Option 2", "Option 3"};
+////    char *colors4[NB_RADIO] = {"#FF0000", "#00FF00", "#0000FF"};
+////    gboolean checked4[NB_RADIO] = {FALSE, TRUE, FALSE};
+////    add_radio(fixed3, NB_RADIO,0,600, labels4, colors4,checked4);
+////    char *labels5[NB_RADIO] = {"Option 1", "Option 2", "Option 3"};
+////    char *colors5[NB_RADIO] = {"#FFFFFF", "#FFFFFF", "#FFFFFF"};
+////    gboolean checked5[NB_RADIO] = {FALSE, FALSE, FALSE};
+////    add_radio(fixed3, NB_RADIO,200,600, labels5, colors5,checked5);
+////    char *labels6[NB_RADIO] = {"Option 1", "Option 2", "Option 3"};
+////    char *colors6[NB_RADIO] = {"#000000", "#000000", "#000000"};
+////    gboolean checked6[NB_RADIO] = {FALSE, FALSE, TRUE};
+////    add_radio(fixed3, NB_RADIO,400,600, labels6, colors6,checked6);
+////    add_spin(fixed3, 0, 800, 0.0, 100.0, 1.0, 1.0, 30, 150, 1, 2, 1, 1, "#00ff00", 1.0);
+////    add_spin(fixed3, 200, 800, 0.0, 100.0, 50.0, 5.0, 30, 30, 1, 2, 1, 1, "#ff0000", 1.0);
+////    add_spin(fixed3, 400, 800, 0.0, 100.0, 100.0, 100.0, 50, 150, 1, 2, 1, 1, "#ffffff", 1.0);
+////
+////    frame* frm=add_frame(fixed,0.5,1.0,txt,NULL,GTK_SHADOW_IN,800,0);
+////    gtk_container_add(GTK_CONTAINER(frm->widget),fixed3);
+/////*************************************frame example fin******************************************************************/
+/////*************************************menu******************************************************************/
+////    GtkWidget* fixed4 = create_fixed();///khawya
+////    Add_Entry(fixed4,"azerty",50,0,1,"A","a",200,50,1,"#FFFFFF",500,500);
+////    add_onglet(notebook,fixed4,"onglet4","#0000FF");
+////    // Ajout de la barre de menus à la fenêtre principale
+////     menubar *maMenuBar = add_menubar(GTK_PACK_DIRECTION_LTR,fixed4);
+////    gtk_fixed_put(fixed4, maMenuBar->menubar,34,34);
+////
+////       menu *menu_file = add_menu(maMenuBar, "File");
+////  // Exemple d'ajout d'éléments de menu à "File"
+////    menu *menu_open = add_menu_item(menu_file, DEFAULT_MENU, "_New", TRUE, FALSE, NULL);
+////       add_menu_item(menu_open, DEFAULT_MENU, "empty file", FALSE, FALSE, NULL);
+////       add_menu_item(menu_open, SEPARATOR_MENU, "sep", FALSE, FALSE, NULL);
+////       add_menu_item(menu_open, DEFAULT_MENU, "project...", FALSE, FALSE, NULL);
+////       add_menu_item(menu_file, DEFAULT_MENU, "_Save", FALSE, FALSE, NULL);
+////
+////    // Exemple d'ajout d'un menu "Edit"
+////    menu *menu_edit = add_menu(maMenuBar, "Edit");
+////        // Exemple d'ajout d'éléments de menu à "Edit"
+////        add_menu_item(menu_edit,DEFAULT_MENU, "C_ut", FALSE, FALSE, NULL);
+////        add_menu_item(menu_edit,DEFAULT_MENU, "_Copy", FALSE, TRUE, NULL);
+////        add_menu_item(menu_edit,DEFAULT_MENU, "_Paste", FALSE, FALSE, NULL);
+////
+////     menu *menu_view = add_menu(maMenuBar, "View");
+////       menu *menu_tool = add_menu_item(menu_view,DEFAULT_MENU, "_Toolbars", TRUE, FALSE, NULL);
+////           add_menu_item(menu_tool,DEFAULT_MENU, "Fit toolbar", FALSE, FALSE, NULL);
+////           add_menu_item(menu_tool,DEFAULT_MENU, "OPtimise toolbar", FALSE, FALSE, NULL);
+////           add_menu_item(menu_tool,SEPARATOR_MENU, NULL, FALSE, FALSE, NULL);
+////           add_menu_item(menu_tool,CHECKBOX_MENU, "Compiler", FALSE, FALSE, NULL);
+////           add_menu_item(menu_tool, CHECKBOX_MENU, "Debuger", FALSE, FALSE, NULL);
+////           add_menu_item(menu_view,CHECKBOX_MENU, "Status_bar", FALSE, FALSE, NULL);
+////       add_menu_item(menu_view,CHECKBOX_MENU, "_Logs", FALSE, FALSE, NULL);
+////       add_menu_item(menu_view,CHECKBOX_MENU, "_Manager", FALSE, FALSE, NULL);
+////       add_menu_item(menu_view,CHECKBOX_MENU, "Status_bar", FALSE, FALSE, NULL);
+////
+////    // Exemple d'ajout d'un menu "Language"
+////    menu *menu_language = add_menu(maMenuBar, "Language");
+////    // Exemple d'ajout d'éléments radio à "Language"
+////       add_menu_item(menu_language, RADIO_MENU, "C/C++", FALSE, TRUE, NULL);
+////       add_menu_item(menu_language, RADIO_MENU, "Python", FALSE, FALSE, NULL);
+////       add_menu_item(menu_language, RADIO_MENU, "Java", FALSE, FALSE, NULL);
+////       add_menu_item(menu_language, RADIO_MENU, "C#", FALSE, FALSE, NULL);
+////       add_menu_item(menu_language, RADIO_MENU, "JavaScript", FALSE, FALSE, NULL);
+////       add_menu_item(menu_language, RADIO_MENU, "PHP", FALSE, FALSE, NULL);
+/////*************************************menu fin******************************************************************/
+////
+/////***************************boite de dilogue*****************************/
+//////    BoiteDialogue* bd=Add_boite_dialogue(GTK_WINDOW(ma_fenetre->window),"exemple boite de dialogue","",0,300,300,0,"red");
+//////    ButtonSimple* bt1=add_button_dialogue(fixed4,"ouvrir boite dialogue","",50,50,"red",200,200,0,0);
+//////    ButtonSimple* bt2=add_button_dialogue(bd->zone_contenu,"fermer","",20,20,"red",10,10,0,0);
+//////    g_signal_connect(bt1->button, "clicked", G_CALLBACK(on_button_clicked), bd->dialogue);
+////
+/////*************************************barre d'outil******************************************************************/
+////    GtkWidget *box=gtk_vbox_new(FALSE,0);
+////    toolbar *ptbar=add_toolbar(box,'v',60,60,60,60,GTK_ICON_SIZE_BUTTON,GTK_TOOLBAR_ICONS,50);
+////    add_item_bar(ptbar,GTK_STOCK_SAVE,"enregistrer",100,100,1);
+////    add_item_bar(ptbar,GTK_STOCK_CLOSE,"fermer",100,100,-1);
+////    add_item_bar(ptbar,GTK_STOCK_QUIT,"quitter",100,100,-1);
+////    add_onglet(notebook,box,"onglet 5","#000000");
+//
+//
+//    ///matqissch hadchi li mor had lcomment
+//    //add_to_window(notebook,ma_fenetre->window);
+//    gtk_widget_show_all(ma_fenetre->window);
+//
+//    fin_programme(ma_fenetre);
+//    return 0;
+//}
+//
+//
+//
--- reader.h
+++ reader.h
@@ -1,30 +1,8 @@
 #ifndef READER_H_INCLUDED
 #define READER_H_INCLUDED
-#include "menu.h"
+
 
 #define MAX 100
-typedef enum{
-    fenetre,
-    Fixed,
-    Label,
-    Entry,
-    Button,
-    Radio,
-    Checkbox,
-    BoiteDialogue,
-    Frame,
-    Image,
-    ProgBar,
-    ScrollBar,
-    spB,
-    ToolBar,
-    ItemBar,
-    MenuBar,
-    Tab,
-    MenuItem,
-    Root
-
-}Token;
 
 
 //typedef struct pl{
@@ -59,122 +37,44 @@ typedef enum{
 //}
 
 
-Token string_to_token(const char *str) {
-    if (!strcmp(str, "window")){
-        return fenetre;
-    }else if (!strcmp(str, "fixed")){
-        return Fixed;
-    } else if (!strcmp(str, "label")) {
-        return Label;
-    } else if (!strcmp(str, "entry")) {
-        return Entry;
-    } else if (!strcmp(str, "button")) {
-        return Button;
-    } else if (!strcmp(str, "button_radio")) {
-        return Radio;
-    } else if (!strcmp(str, "checkbox")) {
-        return Checkbox;
-    } else if (!strcmp(str, "boite_dialogue")) {
-        return BoiteDialogue;
-    } else if (!strcmp(str, "frame")) {
-        return Frame;
-    } else if (!strcmp(str, "image")) {
-        return Image;
-    } else if (!strcmp(str, "prog_bar")) {
-        return ProgBar;
-    } else if (!strcmp(str, "scroll_bar")) {
-        return ScrollBar;
-    } else if (!strcmp(str, "spin_button")) {
-        return spB;
-    } else if (!strcmp(str, "tool_bar")) {
-        return ToolBar;
-    } else if (!strcmp(str, "item_bar")) {
-        return ItemBar;
-    } else if (!strcmp(str, "menu_bar")) {
-        return MenuBar;
-    } else if (!strcmp(str, "Tab")) {
-        return Tab;
-    } else if (!strcmp(str, "menu_item"))
-        return MenuItem;
-      else if (!strcmp(str, "root"))
-        return Root;
 
-     else {
-        // Retourner une valeur par défaut ou une valeur d'erreur
-        return -1;
-    }
-}
-void add_to_parent(GtkWidget* child,GtkWidget* parent_w, gchar* parent,guint posx, guint posy)
-{
-  if(!strcmp(parent,"/fixed"))
-    gtk_fixed_put(GTK_FIXED(parent_w),child,posx,posy);
- else if(!strcmp(parent,"/box"))
-    gtk_box_pack_start(GTK_BOX(parent_w),child,FALSE,FALSE,0);
- else if(!strcmp(parent,"/window"))
-    gtk_container_add(GTK_CONTAINER(parent_w),child);
 
-}
-void lire_fichier(FILE*F,Fenetre* W,GtkWidget* parent_w , gchar* parent_token)
+
+
+void lire_fichier(FILE*F,fixed* fixed0,GtkWidget *pvbox,GtkWidget *pere)
 {
+    if(!F) exit(-1);
     gchar c ;
     gchar current_token[MAX];
     Token tok;
     c=epurer_blan(F);
-    printf("\n%s->%c",parent_token,c);
     while(c!=EOF)
     {
-        printf("\n%s ::: %c",parent_token,c);
+        printf("%c",c);
         if(c=='<')
         {
-              fscanf(F, "%s", current_token);
-              printf("\n%s",current_token);
-
-            if (!strcmp(current_token , parent_token))
-            {
-                if(!(strcmp(current_token,"/root")))
-                  {
-                       printf("\nRRRRRRRRR");
-                       gtk_widget_show_all(W->window);
-                       fin_programme(W);
-                  }
-                c=epurer_blan(F);
-                return ;
-            }
+             fscanf(F,"%s",current_token);
              tok=string_to_token(current_token);
-             switch(tok)
-             {
-               case Root: c=epurer_blan(F);
-                          c=epurer_blan(F);
-
-                   break;
-
-                case fenetre:
-                    printf("\nswitch F");
-                    W=add_window(F);
-                    lire_fichier(F,W,W->window,"/window");
-                    printf("\nsortie F");
-                    c=epurer_blan(F);
-                    break;
-                case Fixed:
-                    fixed* fix=init_gtk_fixed();
-                    add_to_parent(fix->fixed,parent_w,parent_token,0,0);
-                    c=epurer_blan(F);
-                    lire_fichier(F,W,fix->fixed,"/fixed");
-                     printf("\nsortie FIXED");
-                    c=epurer_blan(F);
-
-                    break;
-//                case Label:
-//                    labelFunction(F);
+             switch(tok) {
+//                case fenetre:
+//                    windowFunction(F);
 //                    break;
+//                case Fixed:
+//                    fixedFunction(F);
+//                    break;
+                case Label:
+                   Etiquette *lab= add_label(F);
+                   add_widget_to_fixed(fixed0,lab->widget,lab->x,lab->y);
+                   c=epurer_blan(F);
+                   break;
 //                case Entry:
 //                    entryFunction(F);
 //                    break;
-//                case Button:
-//                    ButtonSimple* B=add_button(F);
-//                    add_widget_to_fixed(fixed0,B->button,50,50);
-//                    c=epurer_blan(F);
-//                    break;
+                case Button:
+                    ButtonSimple* B=add_button(F);
+                    add_widget_to_fixed(fixed0,B->button,50,50);
+                    c=epurer_blan(F);
+                    break;
 //                case Radio:
 //                    buttonRadioFunction(F);
 //                    break;
@@ -184,38 +84,49 @@ void lire_fichier(FILE*F,Fenetre* W,GtkWidget* parent_w , gchar* parent_token)
 //                case BoiteDialogue:
 //                    boiteDialogueFunction(F);
 //                    break;
-//                case Frame:
-//                    frameFunction(F);
-//                    break;
-//                case Image:
-//                    imageFunction(F);
-//                    break;
-//                case ProgBar:
-//                    progBarFunction(F);
-//                    break;
-//                case ScrollBar:
-//                    scrollBarFunction(F);
-//                    break;
+                  case Frame:
+                    frame *fr=add_frame(fixed0->fixed,F);
+                    lire_son_elem(fr,F);
+                    c=epurer_blan(F);
+                    break;
+                 case Image:
+                   image *img=add_image(F);
+                   add_widget_to_fixed(fixed0,img->widget,img->x,img->y);
+                   c=epurer_blan(F);
+                   break;
+                  case ProgBar:
+                      barre_prog *prog_bar=add_progress_bar(F);
+                      add_widget_to_fixed(fixed0,prog_bar->parent,prog_bar->x,prog_bar->y);
+                      c=epurer_blan(F);
+                      break;
+                case ScrollBar:
+                    printf("SDJSJAS");
+                    barre_def *scbar=add_scrollbar(pere,pvbox,F);
+                    c=epurer_blan(F);
+                    break;
 //                case spB:
 //                    spinButtonFunction(F);
 //                    break;
-//                case ToolBar:
-//                    toolBarFunction(F);
-//                    break;
+                case ToolBar:
+                    toolbar *tbar=add_toolbar(pvbox,F);
+                    lire_son_item(tbar ,F);
+                    c=epurer_blan(F);
+                    /*
+                    add_item_bar(tbar,GTK_STOCK_SAVE,"enregistrer",100,100,1);
+                    add_item_bar(tbar,GTK_STOCK_CLOSE,"fermer",100,100,-1);
+                    add_item_bar(tbar,GTK_STOCK_QUIT,"quitter",100,100,-1);
+                    */
+
+                    break;
 //                case ItemBar:
 //                    itemBarFunction(F);
+//                    break;
+//                case MenuBar:
+//                    menuBarFunction(F);
 //                    break;
 //                case Tab:
 //                    ongletFunction(F);
 //                    break;
-                  case MenuBar:
-                        menubar *mbar =add_menubar(mbar,F);
-                         ajouter_elems(mbar,F);
-                        add_to_parent(mbar->menubar,parent_w,parent_token,mbar->posx,mbar->posy);
-                        printf("\nSortirADD");
-                        c=epurer_blan(F);
-
-                        break;
 //                case MenuItem:
 //                    menuItemFunction(F);
 //                    break;
@@ -223,82 +134,13 @@ void lire_fichier(FILE*F,Fenetre* W,GtkWidget* parent_w , gchar* parent_token)
                     // Gérer le cas où le token n'est pas reconnu
                     break;
             }
-
+            printf("\n fin de if");
 
         }
         else
-           {
-             printf("\nssssWWW"); return ;
-           }
-
-
-
-
+                return;
     }
-
 }
-
-//void lire_fichier(FILE *F,Fenetre *Win ,gchar* parent_token)
-//{
-//    gchar c;
-//    gchar current_token[MAX];
-//    Token tok;
-//    c = epurer_blan(F);
-//    while (c != EOF) {
-//        if (c == '<')
-//        {
-//
-//            fscanf(F, "%s", current_token);
-//             if (!strcmp(current_token , parent_token))
-//            {
-//               // if(tok==Root)
-//
-//                return ;
-//            }
-//            printf("\n2222");
-//            tok = string_to_token(current_token);
-//
-//
-//            switch (tok) {
-//            case Root:
-//                   c=epurer_blan(F);
-//                   printf("\n%c",c);
-//                   c=epurer_blan(F);
-//                   printf("\n%c",c);
-//                   break;
-//                case fenetre:
-//                    {
-//                        Win = add_window(F);
-//                        lire_fichier(F,Win,"/window"); // Appel récursif pour lire les descendants de la fenêtre
-//
-//                        break;
-//                    }
-//                case Fixed:
-//                    {
-//                        // Ajoutez ici le code pour gérer le conteneur Fixed
-//                        // Appel récursif pour lire les descendants du conteneur Fixed
-//                        break;
-//                    }
-//
-//                case MenuBar:
-//                    {
-//                        menubar *mbar = add_menubar(mbar, F);
-//                        ajouter_elems(mbar, F);
-//                        // Ajoutez le menu bar à son conteneur parent (s'il y en a un)
-//                        c = epurer_blan(F);
-//                        break;
-//                    }
-//                // Ajoutez des cas pour d'autres conteneurs si nécessaire
-//                default:
-//                    printf("\nDEFAULT");
-//                    break;
-//            }
-//        } else {
-//            return;
-//        }
-//    }
-//}
-//
 
 
 #endif // READER_H_INCLUDED
